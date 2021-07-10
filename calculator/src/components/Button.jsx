b48a776bbdd9d0445b8555ac8fbf508317df6d57
nbroeder <nbroeder@uwo.ca>
2021-07-09 20:17:31 -0400

    Fixed export and import errors

--- calculator/src/components/Button.jsx
+++ calculator/src/components/Button.jsx
@@ -5,7 +5,7 @@ const isOperator=val=>{
     return !isNaN(val)||val==="."||val==="=";
 }
 
-export const Button = props=>(
+  const Button = props=>(
     <div className={`button-wrapper ${
         isOperator(props.children) ? null : "operator"
     }`}>
@@ -13,3 +13,4 @@ export const Button = props=>(
     </div>
 );
 
+export default Button;
